3e3d7fc5fcdbf3ce1b4f3e601ce393bc4a1b0cee
Vineet Agrawal <vineetagrawal59@bitbucket.org>
2016-10-18 11:38:53 +0530

    Allowed map and list to be passed to ngTableParams and display in ngTable

--- src/services/ng.table.service.js
+++ src/services/ng.table.service.js
@@ -3,45 +3,55 @@
 /* Specific to our personal use across projects */
 
 angular
-  .module('angular.extras.thirdparty.services')
-  .factory('NgTableService', ['NgTableParams', function (NgTableParams) {
+    .module('angular.extras.thirdparty.services')
+    .factory('NgTableService', ['NgTableParams', '$q', function (NgTableParams, $q) {
 
-    return {
-      getTableParams: function (model, customNgTableParameters, additionalParams) {
-        var baseNgTableParameters = {
-          page: 1,            // show first page
-          count: 10,
-          sorting: {
-            id: 'asc'
-          }
-        };
+      return {
+        getTableParams: function (model, customNgTableParameters, additionalParams, callback) {
+          var baseNgTableParameters = {
+            page: 1,            // show first page
+            count: 10,
+            sorting: {
+              id: 'asc'
+            }
+          };
 
-        angular.extend(baseNgTableParameters, customNgTableParameters);
+          angular.extend(baseNgTableParameters, customNgTableParameters);
 
-        return new NgTableParams(baseNgTableParameters, {
-          getData: function (params) {
-            var sorting = params.sorting();
-            var sortKey = Object.keys(sorting)[0];
-            var order = sorting[sortKey];
+          return new NgTableParams(baseNgTableParameters, {
+            getData: function (params) {
+              var sorting = params.sorting();
+              var sortKey = Object.keys(sorting)[0];
+              var order = sorting[sortKey];
 
-            var basicParams = {
-              max: params.count(),
-              page: params.page(),
-              filters: params.filter(),
-              sort: sortKey,
-              order: order
-            };
+              var basicParams = {
+                max: params.count(),
+                page: params.page(),
+                filters: params.filter(),
+                sort: sortKey,
+                order: order
+              };
 
-            var queryParams = angular.extend({}, basicParams, additionalParams);
+              var deferred = $q.defer();
 
-            return model.query(queryParams, function (data, headerGetter) {
-              var headers = headerGetter();
-              params.total(headers['total-count']);
+              var queryParams = angular.extend({}, basicParams, additionalParams);
+              model.query(queryParams, function (data, headerGetter) {
+                var headers = headerGetter();
+                params.total(headers['total-count']);
+                console.log('ds');
+                if(callback) {
+                  callback(data);
+                }
+                if(data.result) {
+                  deferred.resolve(data.result);
+                } else {
+                  deferred.resolve(data);
+                }
+              });
 
-              return data;
-            }).$promise;
-          }
-        });
-      }
-    };
-  }]);
+              return deferred.promise;
+            }
+          });
+        }
+      };
+    }]);
